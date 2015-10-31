6240825d60c36bb47fc49bfd2a7a9e407f3f1acd
Bernardo Sulzbach <mafagafogigante@gmail.com>
2015-10-31 14:11:57 -0200

    More automated release script.

--- release.sh
+++ release.sh
@@ -1,8 +1,10 @@
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
     then
-        echo "Pass two arguments: the new tag and a short release message."
+        echo "Just throw in a short release message. I can figure out the tag."
     else
-        git tag -a $1 -m "$2"
+        description=$(git describe --abbrev=0)
+        tag=$(($description + 1))
+        git tag -a $tag -m "$1"
         git push origin HEAD --follow-tags
     # git push origin --set-upstream HEAD
     #   A handy way to push the current branch to the same name on the
