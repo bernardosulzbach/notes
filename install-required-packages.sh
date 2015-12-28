1d2b05294f7024d598c9f20e7acc2b90e93ef501
Bernardo Sulzbach <mafagafogigante@gmail.com>
2015-12-28 17:54:17 -0200

    Added a bash script to install the packages

---
+++ install-required-packages.sh
@@ -0,0 +1,5 @@
+# This script install the required packages in a Debian-based distribution.
+sudo apt-get install -y texlive-latex-extra\
+                        texlive-latex-recommended\
+                        texlive-fonts-recommended\
+                        texlive-math-extra
