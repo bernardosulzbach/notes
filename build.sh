1d23e2bf9065955af738690725537bde0430d5d6
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2018-11-06 19:47:46 -0200

    Using Arara instead of Rubber

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 # Get the first tag found in the history from the current HEAD
 git describe --tags --always --dirty='-*' > .release.txt
-# Use rubber to make a PDF
-rubber --pdf notes.tex
+# Use arara to make a PDF
+arara notes.tex
 # Also make a compressed PDF
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=notes-compressed.pdf notes.pdf
