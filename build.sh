67ad366f094bea30ba2d7856543f18b7eb9ca237
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2018-11-14 17:01:33 -0200

    Release version file should not be hidden

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Get the first tag found in the history from the current HEAD
-git describe --tags --always --dirty='-*' > .release.txt
+git describe --tags --always --dirty='-*' > release.txt
 # Use arara to make a PDF
 arara notes.tex
 # Also make a compressed PDF
